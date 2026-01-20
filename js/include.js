a8d8453859924bc6b67956a8fe64c7fd664deb7d
AsunaYuuki8 <likamiqaia186@gmail.com>
2026-01-20 10:40:15 -0800

    structure & main css for all pg

---
+++ js/include.js
@@ -0,0 +1,16 @@
+
+function includeHTML(selector, file) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) throw new Error("File not found: " + file);
+      return response.text();
+    })
+    .then(data => {
+      document.querySelector(selector).innerHTML = data;
+    })
+    .catch(error => console.log(error));
+}
+
+
+includeHTML("#section1-placeholder", "partials/Section1.html");
+includeHTML("#section7-placeholder", "partials/section7.html");
